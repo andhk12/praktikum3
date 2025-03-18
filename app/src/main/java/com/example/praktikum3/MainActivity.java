057049952f022f3a8095f8c10d04ef8e01ccc3d4
Andhika Pratama Putra <andhikap2323@gmail.com>
2025-03-18 13:16:48 +0700

    v0.3 membuat fitur event handling

--- app/src/main/java/com/example/praktikum3/MainActivity.java
+++ app/src/main/java/com/example/praktikum3/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.praktikum3;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnLogin;
+    EditText etEmail;
+    EditText etPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +27,29 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
+        });
+
+        btnLogin = findViewById(R.id.btn_login);
+        etEmail = findViewById(R.id.et_email);
+        etPassword = findViewById(R.id.et_password);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = etEmail.getText().toString();
+                String password = etPassword.getText().toString();
+
+                if (email.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(MainActivity.this,
+                            "Email dan Password tidak boleh kosong",
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this,
+                            "Berhasil Log In", Toast.LENGTH_SHORT).show();
+                }
+            }
+
         });
     }
 }
